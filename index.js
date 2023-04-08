1d238bd418ba0dd5c1f87e324f4eee2e7103d0a6
QWitcherS <quickwitcher@gmail.com>
2023-04-08 21:48:30 +0500

    Выполнение домашнего задания №2 по JS

---
+++ index.js
@@ -0,0 +1,47 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a); // Задание 1
+
+let b = 2007;
+alert(b); // задание 2
+
+let creatorJavaScript = 'Brendan Eich';
+alert(creatorJavaScript); //Задание 3
+
+let c = 10;
+let d = 2;
+let sum = c + d;
+let dif = c - d;
+let mult = c * d;
+let div = c / d;
+alert('Сумма переменных = ' + sum);
+alert('Разность переменных = ' + dif);
+alert('Произведение переменных = ' + mult);
+alert('Результат от деления = ' + div); //задание 4
+
+let result = d ** 5;
+alert('Результат возведения в степень 2 и 5 = ' + result); //задание 5
+
+a = 9;
+b = 2;
+result = a % b;
+alert('Остаток от деления а на b = ' + result); //задание 6
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
+alert(num); // задание 7
+
+let age = prompt('Сколько вам лет?');
+alert(age); // задание 8
+
+let user = {
+  name:'Bob';
+  age: 18;
+  isAdmin: true;
+};
